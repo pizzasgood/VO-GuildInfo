8e88b15adf3c449fed09a4304eeef3834669ee30
pizzasgood <pizzasgood@gmail.com>
2012-04-16 02:20:16 -0400

    Now pulls and processes main page

--- main.lua
+++ main.lua
@@ -12,13 +12,39 @@ local color = '\127FFFFFF'
 local subcolor = '\127AAAAAA'
 local titlecolor = '\127FFFFAA'
 
+GuildInfo.main_url = 'http://www.vendetta-online.com/x/guildinfo'
+GuildInfo.guilds = {}
+
+function GuildInfo:process_page()
+	if self.main_page == nil then return end
+	print("processing")
+	for link, name, tag, num_members in self.main_page:gmatch("(/x//guildinfo/%d+).->(.-)</a>[^[]*%[(.-)%]</td>.-<td>(%d+)</td>") do
+		self.guilds[tag] = {}
+		self.guilds[tag].link = link
+		self.guilds[tag].name = name
+		self.guilds[tag].tag = tag
+		self.guilds[tag].num_members = num_members
+	end
+end
+
+function GuildInfo:update_links()
+	self.main_page = nil
+	HTTP.urlopen(self.main_url, 'POST', function(success, header, page) 
+			if success ~= nil and header.status == 200 then
+				print("worked")
+				GuildInfo.main_page = page
+				GuildInfo:process_page()
+			else
+				print("Failed")
+				print(success)
+				if header ~= nil then print(header.status) end
+			end
+		end, {})
+end
+
 function GuildInfo.proc(_,data)
 	if (data == nil) then
-		GuildInfo.toggle()
-	elseif (data[1] == "start") then
-		GuildInfo.enable()
-	elseif (data[1] == "stop") then
-		GuildInfo.disable()
+		GuildInfo:update_links()
 	else
 		print(titlecolor.."GuildInfo"..'\127o'..color.." "..string.format("%0.1f",GuildInfo.version)..'\127o')
 		print(color.." Pulls information about guilds from the website."..'\127o')
